cee11522d883920d2dd1df90ed81ad1584266424
Kamiar Kordari <kordari@gmail.com>
2020-03-05 15:18:59 -0800

    feats: slow down the car when too close to the front car

--- src/main.cpp
+++ src/main.cpp
@@ -97,14 +97,37 @@ int main() {
 
           json msgJson;
 
-          //vector<double> next_x_vals;
-          //vector<double> next_y_vals;
-
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
           int prev_size = previous_path_x.size();
+
+
+          if (prev_size > 2)
+          {
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          for (int i = 0; i < sensor_fusion.size(); i++){
+            float d = sensor_fusion[i][6];
+
+            if ((d < (2+4*lane+2)) && (d > (2+4*lane-2)))
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx+vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double)prev_size*0.02*check_speed);
+
+
+              if ((check_car_s > car_s) && ((check_car_s - car_s) < 30))
+              {
+                  ref_vel = 29.5;
+              }
+            }
+          }
+
+
 
           // create a list of widely and evenly spaced waypoints at 30m
           vector<double> ptsx;
