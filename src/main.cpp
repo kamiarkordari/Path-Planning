5213c735535cfd78c9347986913a7a9fd8004ef3
Kamiar Kordari <kordari@gmail.com>
2020-03-05 17:19:52 -0800

    feats: make the car change speed slowly as it gets close to another car

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ int main() {
   int lane = 1;
 
   // reference velocity target (unit: mph)
-  double ref_vel = 49.5;
+  double ref_vel = 0.0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel]
@@ -107,9 +107,11 @@ int main() {
 
           bool too_close = false;
 
+          // loop over other cars
           for (int i = 0; i < sensor_fusion.size(); i++){
             float d = sensor_fusion[i][6];
 
+            // car in the front
             if ((d < (2+4*lane+2)) && (d > (2+4*lane-2)))
             {
               double vx = sensor_fusion[i][3];
@@ -119,15 +121,24 @@ int main() {
 
               check_car_s += ((double)prev_size*0.02*check_speed);
 
-
+              // check if we are too close to the fron car
               if ((check_car_s > car_s) && ((check_car_s - car_s) < 30))
               {
-                  ref_vel = 29.5;
+                  //ref_vel = 29.5;
+                  too_close = true;
               }
             }
           }
 
 
+          if (too_close)
+          {
+            ref_vel -= 0.224;
+          }
+          else if (ref_vel < 49.5)
+          {
+            ref_vel += 0.224;
+          }
 
           // create a list of widely and evenly spaced waypoints at 30m
           vector<double> ptsx;
